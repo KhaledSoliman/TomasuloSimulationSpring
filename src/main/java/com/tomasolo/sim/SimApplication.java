ddd21b8cfedc09d007bbbf44d414503e6a288cdc
Khaled Soliman <khaledsoliman@trukto.com>
2018-12-04 17:53:00 +0200

    Created Test Repo

---
+++ src/main/java/com/tomasolo/sim/SimApplication.java
@@ -0,0 +1,15 @@
+package com.tomasolo.sim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableWebMvc
+public class SimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimApplication.class, args);
+	}
+}
